7af380b0370b665cdaa87217aed144bea72b4b39
Alfandi Alamshach <fandixpc1@gmail.com>
2024-10-13 02:59:27 +0700

    ConditionalExpression1

---
+++ app/src/main/java/ConditionalExpression.kt
@@ -0,0 +1,8 @@
+package com.example.test
+
+// Conditional Expression
+//Conditional expression adalah ekspresi yang mengembalikan nilai berdasarkan kondisi tertentu
+// Conditional expression menggunakan if-else, when, dan try-catch
+fun ConditionalStatement(){
+    println("=== Conditional Expression ===")
+}
