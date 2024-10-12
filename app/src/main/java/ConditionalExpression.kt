11e960c9ca124f7c4177c115490d40081e6ce342
Alfandi Alamshach <fandixpc1@gmail.com>
2024-10-13 03:06:01 +0700

    ConditionalExpression2

--- app/src/main/java/ConditionalExpression.kt
+++ app/src/main/java/ConditionalExpression.kt
@@ -5,4 +5,25 @@ package com.example.test
 // Conditional expression menggunakan if-else, when, dan try-catch
 fun ConditionalStatement(){
     println("=== Conditional Expression ===")
+
+    // If-Else
+    val number = 10
+    if (number > 0) {
+        println("Positive Number")
+    }else {
+        println("Negative Number")
+    }
+
+    //when
+    val day = 1
+    when (day) {
+        1 -> println("Sunday")
+        2 -> println("Monday")
+        3 -> println("Tuesday")
+        4 -> println("Wednesday")
+        5 -> println("Thursday")
+        6 -> println("Friday")
+        7 -> println("Saturday")
+        else -> println("Invalid Day")
+    }
 }
