d567336797ca9789690cd4322a90650ed8f241bc
Alfandi Alamshach <fandixpc1@gmail.com>
2024-10-13 12:43:27 +0700

    Collectin Set

--- app/src/main/java/com/example/project1/Collection.kt
+++ app/src/main/java/com/example/project1/Collection.kt
@@ -28,3 +28,34 @@ fun ContohList() {
     val shapesLocked: List<String> = shape
     println(shapesLocked)
 }
+
+// Set adalah kumpulan data yang tidak memiliki  posisi atau urutan tertentu dan hanya menyimpan data unik
+// Set bersifat unordered, artinya data yang dimasukkan tidak memiliki posisi tertentu
+// Set bersifat mutable dan read only, artinya data yang dimasukkan dapat diubah dan tidak dapat diubah
+// Set Read-Only menggunakan setOf
+// Set Mutable menggunakan mutablesetOf
+
+fun ContohSet() {
+    println()
+    println("=== Set ===")
+
+    //Set Read-Only
+    val readOnlyAbjad = setOf ("A", "B", "C")
+    println(readOnlyAbjad)
+
+    //Set Mutable
+    val shape: MutableSet<String> = mutableSetOf("Circle", "Squere", "Triangle")
+    println(shape)
+
+    //Menambahkan data ke dalam set mutable
+    shape.add("Rectangle")
+    println (shape)
+
+    //Menghapus data dari set mutable
+    shape.remove("Circle")
+    println(shape)
+
+    //Set Read-Only
+    val shapesLocked: Set<String> = shape
+    println (shapesLocked)
+}
